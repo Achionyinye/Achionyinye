4ff5aa25af50304c186909c30da68f772f94e9a0
Achi Onyinye <achionyinyegiftbarbie@gmail.com>
2021-11-04 20:11:47 +0100

    new stuffs

---
+++ index.js
@@ -0,0 +1,20 @@
+
+//console.log("Onyinye");
+
+// function validateForm() {
+//     var x = document.forms["myForm"].value;
+//     if (x == "fname" || x == null) {
+//       alert("Name must be filled out");
+     
+//     }else (x === "address"){
+//         alert("Address must be filled out"); 
+//     }return false;
+//   }
+
+// function validateForm() {
+//     let x = document.forms["miniContainer2"]["firstName"].value;
+//     if (x == "") {
+//       alert("Name must be filled out");
+//       return false;
+//     }
+//   }
