18d700e521810f32638257519639985b344b4886
ikhide <ikhide.atakpu@gmail.com>
2021-11-13 01:44:02 +0100

    error report

--- index.js
+++ index.js
@@ -7,10 +7,36 @@ const password = document.getElementById("password");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  //validate firstname
+  //validate fields
   const validateFirstName = validation.validateFirstName(fname.value);
+  const validateLastName = validation.validateLastName(lastName.value);
+  const validatePassword = validation.validatePassword(password.value);
+  const validateEmail = validation.validateEmail(email.value);
+
   if (validateFirstName.status === false) {
     fname.style.border = "1px solid red";
     document.getElementById("fnameError").innerHTML = validateFirstName.text;
+  }
+
+  if (validateLastName.status === false) {
+    lastName.style.border = "1px solid red";
+    document.getElementById("lastNameError").innerHTML = validateLastName.text;
+  }
+
+  if (validatePassword.status === false) {
+    password.style.border = "1px solid red";
+    document.getElementById("passwordError").innerHTML = validatePassword.text;
+  }
+  if (validateEmail.status === false) {
+    email.style.border = "1px solid red";
+    document.getElementById("emailError").innerHTML = validateEmail.text;
+  }
+  if (
+    validateFirstName.status &&
+    validateLastName.status &&
+    validatePassword.status &&
+    validateEmail.status
+  ) {
+    form.submit();
   }
 });
